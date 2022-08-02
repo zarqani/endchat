bfb099eaad7ba7b0838d915ac13bcd2ab316ef43
f.zarqani <zarqani.f94@gmail.com>
2022-08-02 18:13:02 +0430

    Add frontend

---
+++ frontend/.env.js
@@ -0,0 +1,8 @@
+module.exports = {
+  API_URI: "http://localhost:5411/api",
+  STATIC_URI: "http://localhost:5411/public",
+  STATIC_PHOTOS: "http://localhost:5411/public/images/message",
+  STATIC_FILES: "http://localhost:5411/public/files",
+  STATIC_AVATARS: "http://localhost:5411/public/images/users",
+  SOCKET_ENDPOINT: "http://localhost:5411 ",
+};
